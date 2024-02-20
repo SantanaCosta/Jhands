f1f3e67c3732f0c4a114ba6c08c11444d4bf883c
Yuri S <yuri_s.c@outlook.com>
2024-02-20 16:18:07 -0300

    Add sprite 3

--- js/jhands.js
+++ js/jhands.js
@@ -2,15 +2,20 @@ let initialCenter = window.innerWidth/2.0;
 let lastMouseBody = null;
 let footRot = 0.2;
 let headAngleLimit = 1.0;
+let flagHolding = false;
 let sprites = {
     1: {head: {isRound: true, xScale: 0.1, yScale: 0.1, y: 2, radius: 25, height: null, width: null}, 
-        body: {xScale: 0.1, yScale: 0.1, y: 50, height: 60, width: 40},
+        body: {isUnderHead: true, xScale: 0.1, yScale: 0.1, y: 50, height: 60, width: 40},
         foot: {distance: 15, y: 81, height: 11, width:6, chamfer: {0:[0,3.5,0,0], 1:[3.5,0,0,0]}, 
         fillStyle: '#375b95'}},
-    2: {head: {isRound: true, xScale: 0.08, yScale: 0.08, y: 12, radius: 25, height: null, width: null}, 
-    body: {xScale: 0.1, yScale: 0.1, y: 50, height: 60, width: 40},
-    foot: {distance: 12, y: 78, height: 11, width:6, chamfer: {0:[0,3.5,0,0], 1:[3.5,0,0,0]}, 
-    fillStyle: '#563b4d'}}
+    2: {head: {isRound: true, xScale: 0.08, yScale: 0.08, y: 12, radius: 22, height: null, width: null}, 
+        body: {isUnderHead: true, xScale: 0.1, yScale: 0.1, y: 50, height: 60, width: 40},
+        foot: {distance: 12, y: 78, height: 11, width:6, chamfer: {0:[0,3.5,0,0], 1:[3.5,0,0,0]}, 
+        fillStyle: '#563b4d'}},
+    3: {head: {isRound: false, xScale: 0.12, yScale: 0.12, y: -10, radius: null, height: 50, width: 62}, 
+        body: {isUnderHead: false, xScale: 0.2, yScale: 0.2, y: 50, height: 120, width: 80},
+        foot: {distance: 28, y: 100, height: 18, width:10, chamfer: {0:[0,3.5,0,0], 1:[3.5,0,0,0]}, 
+        fillStyle: '#50355e'}}
 };
 
 function init(world, mouseConstraint){
@@ -107,9 +112,16 @@ function genCharacter(world, mouseConstraint, spriteId) {
 
     let rightFoot = getFoot(spriteId, 1);
     
-    var character = Matter.Body.create({
-        parts: [leftFoot, rightFoot, body, head]
-    });
+    var character;
+
+    if (sprites[spriteId].body.isUnderHead)
+        character = Matter.Body.create({
+            parts: [leftFoot, rightFoot, body, head]
+        });
+    else
+        character = Matter.Body.create({
+            parts: [leftFoot, rightFoot, head, body]
+        });
 
     var partsQty = character.parts.length;
 
@@ -121,38 +133,60 @@ function genCharacter(world, mouseConstraint, spriteId) {
         Matter.Body.setAngle(head, character.angle);
         Matter.Body.setAngle(body, character.angle);
         
-        if(lastMouseBody === character && character.parts.length >= partsQty){
-            
-            // Head rotation when grabbing
-            var headRotSpeed = 0.05;
-            if(++headAngleLimit > 2)
-                headAngleLimit *= -1;
+        if(character.parts.length >= partsQty){
 
-            // rotaciona a head com um ângulo relativo ao seu ângulo atual
-            Matter.Body.rotate(head, headRotSpeed * headAngleLimit);
+            // Character tends to stand
+            if(Matter.Body.getSpeed(character) <= 0.1){
+                if (character.angle > 0)
+                    character.torque = -0.2;
+                else if (character.angle < 0)
+                    character.torque = 0.2;
 
-            if (leftFoot.angle > 1.5)
-                footRot = -0.15;
-            else if (leftFoot.angle < 0)
-                footRot = 0.15;
+                Matter.Body.setAngularVelocity(character, Math.min(Math.max(character.angularVelocity, -0.1), 0.1));
+            }
 
-            Matter.Body.rotate(leftFoot, footRot);
-            Matter.Body.rotate(rightFoot, footRot*-1);
-        }
+            if(flagHolding && lastMouseBody === character){
+                
+                // Head rotation when grabbing
+                var headRotSpeed = 0.05;
+                if(++headAngleLimit > 2)
+                    headAngleLimit *= -1;
+
+                // rotaciona a head com um ângulo relativo ao seu ângulo atual
+                Matter.Body.rotate(head, headRotSpeed * headAngleLimit);
+
+                if (leftFoot.angle > 1.5)
+                    footRot = -0.15;
+                else if (leftFoot.angle < 0)
+                    footRot = 0.15;
+
+                Matter.Body.rotate(leftFoot, footRot);
+                Matter.Body.rotate(rightFoot, footRot*-1);
+            }
+    }
+    });
+
+    function restoreDefaultBody(){
+        head.render.sprite.texture = "assets/Sprite" + spriteId + "-Head.png"
+        Matter.Body.setAngle(leftFoot, 0);
+        Matter.Body.setAngle(rightFoot, 0);
+    }
+
+    Matter.Events.on(mouseConstraint, 'mouseup', function(event) {
+        flagHolding = false;
+        restoreDefaultBody();   
     });
 
     Matter.Events.on(mouseConstraint, 'mousedown', function(event) {
         // Saving body being held
         if (event.mouse.button != 2){
+            flagHolding = true;
             lastMouseBody = mouseConstraint.body;
 
             if (lastMouseBody === character) // Changing head sprite when being held
                 head.render.sprite.texture = "assets/Sprite" + spriteId + "-HeadGrab.png";
-            else if (character.parts.length >= partsQty){ // Restoring default body, if alive
-                head.render.sprite.texture = "assets/Sprite" + spriteId + "-Head.png"
-                Matter.Body.setAngle(leftFoot, 0);
-                Matter.Body.setAngle(rightFoot, 0);
-            }
+            else if (character.parts.length >= partsQty) // Restoring default body, if alive
+                restoreDefaultBody(); 
         } 
 
         // Head cutting
